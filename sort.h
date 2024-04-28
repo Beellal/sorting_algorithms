30f8cff79db3f11c06a6e7973930ee36a681fbd2
Beellal <mahamanehabiboubilal999@gmail.com>
2024-04-28 03:55:58 -0700

    Quick Sort

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,6 +22,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 
