5ec2ce4fb763e54e39bd0d7ef5579cb7e6100585
Beellal <mahamanehabiboubilal999@gmail.com>
2024-05-02 03:59:54 -0700

    Shell Sorrt

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define _SORT_H_
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,7 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+void swap(int *array, int item1, int item2);
 
 
 #endif /*_SORT_H_*/
